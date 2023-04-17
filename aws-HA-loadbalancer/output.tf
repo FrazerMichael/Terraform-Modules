faa387efb302b2077e7c853b3d98bec7d5de2b0e
EC2 Default User <ec2-user@ip-172-31-47-101.ec2.internal>
2023-04-17 00:03:37 +0000

    fix HA-vpc output

--- aws-HA-loadbalancer/output.tf
+++
@@ -1,3 +0,0 @@
-output "subnet-ids" {
-  value = [aws_subnet.SN-1.id, aws_subnet.SN-2.id, aws_subnet.SN-3.id]
-}
--- aws-HA-vpc/outputs.tf
+++ aws-HA-vpc/outputs.tf
@@ -1,3 +1,7 @@
 output "vpc-id" {
   value = aws_vpc.vpc.id
 }
+
+output "subnet-ids" {
+  value = [aws_subnet.SN-1.id, aws_subnet.SN-2.id, aws_subnet.SN-3.id]
+}
